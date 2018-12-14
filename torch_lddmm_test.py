93c563ec532c229854b2bd8886a021a4f356aaf5
Brian C. Lee <leebc@io16>
2018-12-14 17:23:47 -0500

    changed default to gpu, removed old code

--- torch_lddmm_test.py
+++ torch_lddmm_test.py
@@ -3,5 +3,5 @@ import torch_lddmm
 template_file_name = '/cis/home/leebc/Projects/Mouse_Histology/data/registration/Hua121/Hua121_affine.img'
 target_file_name = '/cis/home/leebc/Projects/Mouse_Histology/data/registration/Hua121/Hua121_targetmasked.img'
 
-lddmm = torch_lddmm.LDDMM(template=template_file_name,target=target_file_name,outdir='/cis/home/leebc/Projects/Mouse_Histology/data/registration/torch_test/',gpu_number=None,a=3.0,p=2,niter=100,epsilon=5e-3,sigma=2.0,sigmaR=1.0,nt=5)
+lddmm = torch_lddmm.LDDMM(template=template_file_name,target=target_file_name,outdir='/cis/home/leebc/Projects/Mouse_Histology/data/registration/torch_test/',gpu_number=0,a=3.0,p=2,niter=100,epsilon=5e-3,sigma=2.0,sigmaR=1.0,nt=5)
 lddmm.run()
